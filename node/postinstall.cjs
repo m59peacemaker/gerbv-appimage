04c809a71b38a0792f0027d1a578a8959b101b51
Johnny Hauser <johnnyhauser@gmail.com>
2020-07-06 15:36:31 -0500

    trying another way to bin link to the binary - using a placeholder and switching it out for the real binary in postinstall

--- node/postinstall.cjs
+++ node/postinstall.cjs
@@ -3,17 +3,28 @@
 const { execFileSync } = require('child_process')
 const fs = require('fs')
 const path = require('path')
+const binLinks = require('bin-links')
 const download = require('download')
+const readPkg = require('read-package-json-fast')
 
 ;(async () => {
-	const url = (await fs.promises.readFile(`${__dirname}/../AppImage_url`)).replace('+', encodeUriComponent('+'))
-	const appimage = `${__dirname}/.tmp/gerbv/gerbv`
+	const url = (await fs.promises.readFile(`${__dirname}/../AppImage_url`, 'utf8')).replace('+', encodeURIComponent('+'))
+	const appimage = `${__dirname}/bin/gerbv/gerbv_AppImage`
+	const root = `${__dirname}/../`
 
 	console.log(`Downloading ${url} to ${appimage}...`)
-
 	fs.promises.mkdir(path.dirname(appimage), { recursive: true })
 	await fs.promises.writeFile(appimage, await download(url))
 
 	execFileSync('chmod', [ 'a+x', appimage ])
-	execFileSync(appimage, [ '--appimage-extract' ])
+
+	console.log(`extracting AppImage...`)
+	execFileSync(appimage, [ '--appimage-extract' ], { cwd: path.dirname(appimage) })
+
+	console.log('linking bin...')
+	await binLinks({
+		path: root,
+		pkg: await readPkg(`${root}/package.json`),
+		force: true
+	})
 })()
